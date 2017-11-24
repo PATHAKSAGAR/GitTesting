349715e179d2ceef61331f9a4101db6c251b5d88
PATHAKSAGAR <sagar18889@outlook.com>
2017-11-24 13:35:26 +0530

    testing

---
+++ WordCount/Controllers/Constants.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WordCount.Controllers
+{
+    public class Constants
+    {
+
+        public const string SPACE=" ";
+        public const string REPLACESPACE = "";
+        public const int MAXLENGTH = 500;
+        public const string LINEBREAK = "<br />";
+        public const string SEPRATOR = " = ";
+    }
+}
---
+++ WordCount/Controllers/CountController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Data.Entity;
+using System.Text.RegularExpressions;
+
+namespace WordCount.Controllers
+{
+    public class CountController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(string txtString)
+        {
+            Dictionary<string, int> wordCount = new Dictionary<string, int>();
+
+            // Declare array and sorted 
+            string[] words = { "Bike", "C++", "C#", "VB", "Java", "Ruby", "Physics", "Chemistry", "Mathematics", "Social Science", "History", "Geography", "Rickshaw", "Bus", "Train","cycle" };  //Declaration of array
+            Array.Sort(words);
+            for (int i = 0; i < words.Length; i++)
+            {
+                int count = 0;
+                string Sentence = string.Empty;
+                // Ignored max legth
+                Sentence = (Convert.ToInt32(txtString.Length) >= Constants.MAXLENGTH) ? txtString.Substring(0, Constants.MAXLENGTH).Replace(Constants.SPACE, Constants.REPLACESPACE) : Sentence = txtString.Replace(Constants.SPACE, Constants.REPLACESPACE);
+                if (Sentence.ToLower().Contains(words[i].Replace(Constants.SPACE, Constants.REPLACESPACE).ToLower()))
+                {
+                    count = (Sentence.ToLower().Split(new string[] { words[i].Replace(Constants.SPACE, Constants.REPLACESPACE).ToLower() }, StringSplitOptions.None).Length) - 1;
+                    //results += words[i] + Constants.SEPRATOR + Convert.ToString(count) + Constants.LINEBREAK;
+                    wordCount.Add(words[i], count);
+                }
+            }
+            ViewBag.result = wordCount;
+            return View();
+        }
+    }
+}
